a919ba70096b4b7008fdc7e393a338ac363f586d
nurandrn <45285093+nurandrn@users.noreply.github.com>
2019-07-05 00:40:22 +0300

    Add files via upload

---
+++ XMLVeriİslemleri/XMLVeriİslemleri/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace XMLVeriİslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnVeriOku = new System.Windows.Forms.Button();
+            this.btnAddChild = new System.Windows.Forms.Button();
+            this.btnVeriBul = new System.Windows.Forms.Button();
+            this.btnSQLtoXML = new System.Windows.Forms.Button();
+            this.btnXPathileTagBul = new System.Windows.Forms.Button();
+            this.btnXMLtoSQL = new System.Windows.Forms.Button();
+            this.btnXPathileVerileriBul = new System.Windows.Forms.Button();
+            this.btnSchemaOlustur = new System.Windows.Forms.Button();
+            this.btnVeriDegistir = new System.Windows.Forms.Button();
+            this.btnSQLdenSchema = new System.Windows.Forms.Button();
+            this.btnXMLVeriSil = new System.Windows.Forms.Button();
+            this.btnXMLSchemaDogrula = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(435, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(353, 426);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(412, 217);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnVeriOku
+            // 
+            this.btnVeriOku.Location = new System.Drawing.Point(17, 235);
+            this.btnVeriOku.Name = "btnVeriOku";
+            this.btnVeriOku.Size = new System.Drawing.Size(168, 30);
+            this.btnVeriOku.TabIndex = 2;
+            this.btnVeriOku.Text = "XML Dosyadan Veri Oku";
+            this.btnVeriOku.UseVisualStyleBackColor = true;
+            this.btnVeriOku.Click += new System.EventHandler(this.btnVeriOku_Click);
+            // 
+            // btnAddChild
+            // 
+            this.btnAddChild.Location = new System.Drawing.Point(241, 235);
+            this.btnAddChild.Name = "btnAddChild";
+            this.btnAddChild.Size = new System.Drawing.Size(168, 30);
+            this.btnAddChild.TabIndex = 2;
+            this.btnAddChild.Text = "XML\'de veri ekle";
+            this.btnAddChild.UseVisualStyleBackColor = true;
+            this.btnAddChild.Click += new System.EventHandler(this.btnAddChild_Click);
+            // 
+            // btnVeriBul
+            // 
+            this.btnVeriBul.Location = new System.Drawing.Point(17, 271);
+            this.btnVeriBul.Name = "btnVeriBul";
+            this.btnVeriBul.Size = new System.Drawing.Size(168, 30);
+            this.btnVeriBul.TabIndex = 2;
+            this.btnVeriBul.Text = "XML İçindeki Veriyi Bul";
+            this.btnVeriBul.UseVisualStyleBackColor = true;
+            this.btnVeriBul.Click += new System.EventHandler(this.btnVeriBul_Click);
+            // 
+            // btnSQLtoXML
+            // 
+            this.btnSQLtoXML.Location = new System.Drawing.Point(241, 271);
+            this.btnSQLtoXML.Name = "btnSQLtoXML";
+            this.btnSQLtoXML.Size = new System.Drawing.Size(168, 30);
+            this.btnSQLtoXML.TabIndex = 2;
+            this.btnSQLtoXML.Text = "VeriTabanından XML\'e oku";
+            this.btnSQLtoXML.UseVisualStyleBackColor = true;
+            this.btnSQLtoXML.Click += new System.EventHandler(this.btnSQLtoXML_Click);
+            // 
+            // btnXPathileTagBul
+            // 
+            this.btnXPathileTagBul.Location = new System.Drawing.Point(17, 307);
+            this.btnXPathileTagBul.Name = "btnXPathileTagBul";
+            this.btnXPathileTagBul.Size = new System.Drawing.Size(168, 30);
+            this.btnXPathileTagBul.TabIndex = 2;
+            this.btnXPathileTagBul.Text = "XML\'de Veriyi XPath ile Bul";
+            this.btnXPathileTagBul.UseVisualStyleBackColor = true;
+            this.btnXPathileTagBul.Click += new System.EventHandler(this.btnXPathileTagBul_Click);
+            // 
+            // btnXMLtoSQL
+            // 
+            this.btnXMLtoSQL.Location = new System.Drawing.Point(241, 307);
+            this.btnXMLtoSQL.Name = "btnXMLtoSQL";
+            this.btnXMLtoSQL.Size = new System.Drawing.Size(168, 30);
+            this.btnXMLtoSQL.TabIndex = 2;
+            this.btnXMLtoSQL.Text = "XML\'de VeriTabanına Yaz";
+            this.btnXMLtoSQL.UseVisualStyleBackColor = true;
+            this.btnXMLtoSQL.Click += new System.EventHandler(this.btnXMLtoSQL_Click);
+            // 
+            // btnXPathileVerileriBul
+            // 
+            this.btnXPathileVerileriBul.Location = new System.Drawing.Point(17, 343);
+            this.btnXPathileVerileriBul.Name = "btnXPathileVerileriBul";
+            this.btnXPathileVerileriBul.Size = new System.Drawing.Size(168, 30);
+            this.btnXPathileVerileriBul.TabIndex = 2;
+            this.btnXPathileVerileriBul.Text = "XML\'de verileri XPath ile Bul";
+            this.btnXPathileVerileriBul.UseVisualStyleBackColor = true;
+            this.btnXPathileVerileriBul.Click += new System.EventHandler(this.btnXPathileVerileriBul_Click);
+            // 
+            // btnSchemaOlustur
+            // 
+            this.btnSchemaOlustur.Location = new System.Drawing.Point(241, 343);
+            this.btnSchemaOlustur.Name = "btnSchemaOlustur";
+            this.btnSchemaOlustur.Size = new System.Drawing.Size(168, 30);
+            this.btnSchemaOlustur.TabIndex = 2;
+            this.btnSchemaOlustur.Text = "XML Schema Tanımı Oluştur";
+            this.btnSchemaOlustur.UseVisualStyleBackColor = true;
+            this.btnSchemaOlustur.Click += new System.EventHandler(this.btnSchemaOlustur_Click);
+            // 
+            // btnVeriDegistir
+            // 
+            this.btnVeriDegistir.Location = new System.Drawing.Point(17, 379);
+            this.btnVeriDegistir.Name = "btnVeriDegistir";
+            this.btnVeriDegistir.Size = new System.Drawing.Size(168, 30);
+            this.btnVeriDegistir.TabIndex = 2;
+            this.btnVeriDegistir.Text = "XML İçindeki veriyi değiştir";
+            this.btnVeriDegistir.UseVisualStyleBackColor = true;
+            this.btnVeriDegistir.Click += new System.EventHandler(this.btnVeriDegistir_Click);
+            // 
+            // btnSQLdenSchema
+            // 
+            this.btnSQLdenSchema.Location = new System.Drawing.Point(241, 379);
+            this.btnSQLdenSchema.Name = "btnSQLdenSchema";
+            this.btnSQLdenSchema.Size = new System.Drawing.Size(168, 30);
+            this.btnSQLdenSchema.TabIndex = 2;
+            this.btnSQLdenSchema.Text = "SQL\'den istenen şemaya uygun XML oluştur";
+            this.btnSQLdenSchema.UseVisualStyleBackColor = true;
+            this.btnSQLdenSchema.Click += new System.EventHandler(this.btnSQLdenSchema_Click);
+            // 
+            // btnXMLVeriSil
+            // 
+            this.btnXMLVeriSil.Location = new System.Drawing.Point(17, 415);
+            this.btnXMLVeriSil.Name = "btnXMLVeriSil";
+            this.btnXMLVeriSil.Size = new System.Drawing.Size(168, 30);
+            this.btnXMLVeriSil.TabIndex = 2;
+            this.btnXMLVeriSil.Text = "XML\'den Veri Sil";
+            this.btnXMLVeriSil.UseVisualStyleBackColor = true;
+            this.btnXMLVeriSil.Click += new System.EventHandler(this.btnXMLVeriSil_Click);
+            // 
+            // btnXMLSchemaDogrula
+            // 
+            this.btnXMLSchemaDogrula.Location = new System.Drawing.Point(241, 415);
+            this.btnXMLSchemaDogrula.Name = "btnXMLSchemaDogrula";
+            this.btnXMLSchemaDogrula.Size = new System.Drawing.Size(168, 30);
+            this.btnXMLSchemaDogrula.TabIndex = 2;
+            this.btnXMLSchemaDogrula.Text = "XML Schema Doğrulama";
+            this.btnXMLSchemaDogrula.UseVisualStyleBackColor = true;
+            this.btnXMLSchemaDogrula.Click += new System.EventHandler(this.btnXMLSchemaDogrula_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnXMLSchemaDogrula);
+            this.Controls.Add(this.btnSchemaOlustur);
+            this.Controls.Add(this.btnSQLtoXML);
+            this.Controls.Add(this.btnXMLVeriSil);
+            this.Controls.Add(this.btnXPathileVerileriBul);
+            this.Controls.Add(this.btnVeriBul);
+            this.Controls.Add(this.btnSQLdenSchema);
+            this.Controls.Add(this.btnXMLtoSQL);
+            this.Controls.Add(this.btnVeriDegistir);
+            this.Controls.Add(this.btnXPathileTagBul);
+            this.Controls.Add(this.btnAddChild);
+            this.Controls.Add(this.btnVeriOku);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "XML Dosyadan Veri Oku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnVeriOku;
+        private System.Windows.Forms.Button btnAddChild;
+        private System.Windows.Forms.Button btnVeriBul;
+        private System.Windows.Forms.Button btnSQLtoXML;
+        private System.Windows.Forms.Button btnXPathileTagBul;
+        private System.Windows.Forms.Button btnXMLtoSQL;
+        private System.Windows.Forms.Button btnXPathileVerileriBul;
+        private System.Windows.Forms.Button btnSchemaOlustur;
+        private System.Windows.Forms.Button btnVeriDegistir;
+        private System.Windows.Forms.Button btnSQLdenSchema;
+        private System.Windows.Forms.Button btnXMLVeriSil;
+        private System.Windows.Forms.Button btnXMLSchemaDogrula;
+    }
+}
+
---
+++ XMLVeriİslemleri/XMLVeriİslemleri/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Xml.XPath;
+using System.Xml;
+
+namespace XMLVeriİslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosyaYolu = Application.StartupPath + "\\CalisanListesi.xml";
+
+        const string veritabani = @"Data Source=.;Initial Catalog=Northwind;Integrated Security=True";
+         //server=.;Database=Northwind;trusted_connection=true";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri(dosyaYolu);
+        }
+
+
+        private void btnVeriOku_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(dosyaYolu);
+            dataGridView1.DataSource = ds.Tables[0];
+            webBrowser1.Url = new Uri(dosyaYolu);
+
+        }
+
+        private void btnVeriBul_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dosyaYolu);
+            XmlNode secilenNode = xmlDoc.ChildNodes[1];
+            bool bulundu = false;
+            foreach (XmlNode item in secilenNode)
+            {
+                if (item.ChildNodes[0].InnerText == "Can"||item.Attributes["TCNo"].Value == "12345678901")
+                {
+                    MessageBox.Show("Aranan kisi foreach yaparak bulundu:\n\n" +item.ChildNodes[0].InnerText+" " +item.ChildNodes[1].InnerText + "\n" +item.ChildNodes[2].InnerText + "\n" +"TCNo: " +item.Attributes["TCNo"].Value);
+                    bulundu = true;
+                    break;
+                }
+
+            }
+            if (!bulundu)
+            {
+                MessageBox.Show("Aranan kişi 'Can' bulunamadı.", " ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnXPathileTagBul_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dosyaYolu);
+            XmlNode secilenNode =
+            xmlDoc.SelectSingleNode("Calisanlar / Calisan[Adi = 'Melek']");
+            xmlDoc.SelectSingleNode(" Calisanlar / Calisan[@TCNo = 12345678902] ");
+            if (secilenNode != null)
+            {
+                MessageBox.Show(" Aranan kisi XPath ile kolayca bulundu:\n\n "
+                +secilenNode.ChildNodes[0].InnerText + " "
+                +secilenNode.ChildNodes[1].InnerText + "\n"
+                +secilenNode.ChildNodes[2].InnerText + "\n"
+                +"TCNo:" +secilenNode.Attributes["TCNo"].Value);
+            }
+            else
+            {
+                MessageBox.Show(" Aranan kisi 'Melek'; bulunamadi." , "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            webBrowser1.Url = new Uri(dosyaYolu);
+        }
+
+        private void btnXPathileVerileriBul_Click(object sender, EventArgs e)
+        {
+            XPathDocument xmlDoc = new XPathDocument(dosyaYolu);
+            XPathNavigator xNav = xmlDoc.CreateNavigator();
+            XPathNodeIterator secilenNode = xNav.Select("Calisanlar/Calisan/Adi");
+
+            string metin = "";
+            while (secilenNode.MoveNext())
+            {
+                if (secilenNode.Current.InnerXml.StartsWith("C"))
+                    metin += secilenNode.Current.InnerXml + "\n";
+            }
+            if (metin != "")
+                MessageBox.Show("Adi C ile baslayanlar:\n\n" + metin);
+            else
+                MessageBox.Show("Adi C ile baslayan bulunamadi");
+            webBrowser1.Url = new Uri(dosyaYolu);
+        }
+
+        private void btnVeriDegistir_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dosyaYolu);
+            XmlNode secilenNode = xmlDoc.SelectSingleNode("Calisanlar/Calisan[Adi='Melek']");
+            if (secilenNode != null)
+            {
+                secilenNode.ChildNodes[1].InnerText = " Polat";
+                secilenNode.ChildNodes[1].InnerText += " Galipler";
+                xmlDoc.Save(dosyaYolu);
+
+                MessageBox.Show("Soyadına eklemek ekleme yapıldı: \n \n"
+                    + secilenNode.ChildNodes[0].InnerText + ""
+                    + secilenNode.ChildNodes[1].InnerText);
+            }
+            else
+            {
+                MessageBox.Show("Aranan kişi 'Melek' bulunamadı.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            webBrowser1.Url = new Uri(dosyaYolu);
+        }
+
+        private void btnXMLVeriSil_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dosyaYolu);
+            XmlNode secilenNode = xmlDoc.SelectSingleNode("Calisanlar/Calisan[Adi='Melek']");
+            if (secilenNode != null)
+            {
+                xmlDoc.DocumentElement.RemoveChild(secilenNode);
+                xmlDoc.Save(dosyaYolu);
+                MessageBox.Show("Melek dosyadan silindi.");
+            }
+            else
+            {
+                MessageBox.Show("Aranan kişi 'Melek' bulunamadı.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            webBrowser1.Url = new Uri(dosyaYolu);
+
+        }
+
+        private void btnAddChild_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+       
+            xmlDoc.Load(dosyaYolu);
+
+            XmlElement yeniEleman = xmlDoc.CreateElement("Calisan");
+
+            XmlAttribute attrTCNo = xmlDoc.CreateAttribute("TCNo");
+            attrTCNo.Value = "1234678904";
+            yeniEleman.Attributes.Append(attrTCNo);
+
+            XmlNode xAdi = xmlDoc.CreateElement("Adi");
+            xAdi.InnerText = "Buse";
+            yeniEleman.AppendChild(xAdi);
+
+            XmlNode xSoyadi = xmlDoc.CreateElement("Soyadi");
+            xSoyadi.InnerText = "Zengin";
+            yeniEleman.AppendChild(xSoyadi);
+
+            xmlDoc.DocumentElement.AppendChild(yeniEleman);
+            xmlDoc.Save(dosyaYolu);
+            MessageBox.Show("'Buse' eklendi");
+            webBrowser1.Url = new Uri(dosyaYolu);
+
+
+        }
+
+        private void btnSQLtoXML_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection(veritabani);
+            SqlDataAdapter adp = new SqlDataAdapter("select * from Products", cnn);
+
+            DataTable dt = new DataTable("Product");
+            adp.Fill(dt);
+
+            DataSet ds = new DataSet("Products");
+            ds.Tables.Add(dt);
+
+           // string dosya = Application.StartupPath + "\\SQLtoXML.xml";
+
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            DialogResult dr = fd.ShowDialog();
+            if (dr != DialogResult.OK)
+                return;
+            string dosya = fd.SelectedPath + "\\SQLtoXML.xml";
+            ds.WriteXml(dosya);
+            MessageBox.Show("SQL'den gelen veriler XML dosyasına yazildi.\n" + dosya);
+            webBrowser1.Url = new Uri(dosya);
+
+        }
+
+        private void btnXMLtoSQL_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            DialogResult dr = fd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string dosya = fd.SelectedPath + "\\SQLtoXML.xml";
+                if (File.Exists(dosya))
+                {
+                    SqlConnection cnn = new SqlConnection(veritabani);
+                    SqlDataAdapter adp = new SqlDataAdapter("select * from ProductsX", cnn);
+                    SqlCommandBuilder cb = new SqlCommandBuilder(adp);
+                    DataSet ds = new DataSet();
+
+                    ds.ReadXml(dosya);
+                    adp.Update(ds.Tables[0]);
+
+                    MessageBox.Show("XML'den okunan veriler ProductsX tablosuna kaydedildi.");
+                    webBrowser1.Url = new Uri(dosya);
+                }
+                else
+                {
+                    MessageBox.Show("Dosya bulunamadı. \n" + dosya, "",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+
+                }
+                
+            }
+        }
+
+        private void btnSchemaOlustur_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSQLdenSchema_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXMLSchemaDogrula_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
